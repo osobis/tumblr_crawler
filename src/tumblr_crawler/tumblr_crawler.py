db11f373e0c5908bfb32f3b5ff2a101fd79d163b
Leszek Skoczylas <osobis@gmail.com>
2011-04-09 10:31:11 +0300

    Doing major refactoring

--- src/tumblr_crawler/tumblr_crawler.py
+++ src/tumblr_crawler/tumblr_crawler.py
@@ -8,96 +8,96 @@ import ConfigParser
 import logging
 import urllib2
 import os
+import Queue
 from cutils import get_from_config
 from urlparse import urlparse
     
-PHOTO_DATA_LOCK = threading.Lock()
+CONTEXT_LOCK = threading.Lock()
     
 class TumblrImageCrawler(threading.Thread):
     
-    def __init__(self, name, photo_data,  email = None, password = None, tags = None, max=50):
+    def __init__(self, name, task_queue, context,  email = None, password = None, tags = None, max=50, limit=0):
         
         self.__log = logging.getLogger(self.__class__.__name__)
-        self.__photo_data = photo_data
+        self.__task_queue = task_queue
+        self.__context = context
         self.__name = name
         self.__email = email
         self.__password = password
         self.__tags = tags
         self.__max = max
+        self.__limit = limit
         self.__api = tumblr.Api(name, email, password, None, None, tags, None)
         threading.Thread.__init__(self)
+        
+    def __process_search_results(self, results):
+        
+        photo_data = dict()
+        count = 0
+        for result in results:
+            if isinstance(result, dict):
+                photo_caption = result.get('photo-caption')
+                if photo_caption:
+                    photo_data[photo_caption] = dict()
+                    for key in result.keys():
+                        if key.startswith('photo-url-'):
+                            try:
+                                new_key = int(key.split('photo-url-')[-1])
+                                photo_data[photo_caption][new_key] = result[key]
+                            except ValueError:
+                                self.__log.warning('Ooops! Strange photo-url key: %r', key)
+            else:
+                self.__log.warning('Result: %r not a dict', result)
+            count += 1
+        return photo_data, count                
         
     def run(self):
         start = 0
         while True:
             try:
-                self.__log.info('Starting with: %d; max: %d for name: %r', start, self.__max, self.__name)
+                self.__log.info('Starting with: %d; max: %d for name: %r with limit: %d', start, self.__max, self.__name, self.__limit)
                 results = self.__api.read(start = start, max = self.__max)
-                count = 0
-                photo_data = dict()
-                for result in results:
-                    count += 1
-                    if isinstance(result, dict):
-                        photo_caption = result.get('photo-caption')
-                        if photo_caption:
-                            photo_data[photo_caption] = dict()
-                            for key in result.keys():
-                                if key.startswith('photo-url-'):
-                                    try:
-                                        new_key = int(key.split('photo-url-')[-1])
-                                        photo_data[photo_caption][new_key] = result[key]
-                                    except ValueError:
-                                        self.__log.warning('Ooops! Strange photo-url key: %r', key)
-                    else:
-                        self.__log.warning('Result: %r not a dict', result)
-                        
+                photo_data, count = self.__process_search_results(results)
+                
                 if count == 0:
                     self.__log.info('No more results for name: %r', self.__name)
                     break
-                
-                with PHOTO_DATA_LOCK:       
-                    self.__log.debug('Updating photo_data for name: %r', self.__name)       
-                    self.__photo_data.update(photo_data)
+                       
+                if photo_data:      
+                    self.__log.debug('Putting photo_data on the queue for name: %r', self.__name) 
+                    self.__task_queue.put_nowait(photo_data)
+                                
                 start += self.__max
+                
+                if self.__limit > 0:
+                    if start >= self.__limit*self.__max:
+                        self.__log.info('Reached the limit of %d batches' % self.__limit)
+                        break
+                
             except StandardError, error:
                 self.__log.error('Error: %r', error)
                 
             self.__log.info('Sleeping for name: %r', self.__name)
-            time.sleep(0.5)
+            time.sleep(0.2)
             
-
+        with CONTEXT_LOCK:
+            self.__context['count_done_crawler'] += 1
+     
+            
+class PhotoSaveThread(threading.Thread):
     
-    
-class TumblrService(object):
-    
-    def __init__(self, config_file):
+    def __init__(self, config, task_queue, context):
         
-        self.__config_file = config_file
-        self.__config = ConfigParser.SafeConfigParser()
-        self.__config.read(config_file)
-        self.__get_logging()
-        self.__me_name = get_from_config(self.__config, 'TUMBLR', 'me_name')
-        self.__me__pasword = get_from_config(self.__config, 'TUMBLR', 'me_password')
-        self.__me_mail = get_from_config(self.__config, 'TUMBLR', 'me_email')
-        self.__me_max = get_from_config(self.__config, 'TUMBLR', 'me_max')
-        self.__acounts = [item.strip() for item in get_from_config(self.__config, 'TUMBLR', 'accounts').split(',')]
-        self.__save_to_dir = get_from_config(self.__config, 'TUMBLR', 'save_to_dir')
-        self.__photo_data = dict()
-        self.__image_crawlers = list()
-        for account in self.__acounts:
-            self.__image_crawlers.append(TumblrImageCrawler(photo_data=self.__photo_data, name = account, max = self.__me_max))
-        self.__log.info('I am: %r', self.__class__.__name__)
-        
-    def __get_logging(self):
-        
-        logging.basicConfig(level = getattr(logging, self.__config.get('TUMBLR', 'logging_level')),
-                            format = '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+        self.__config = config
         self.__log = logging.getLogger(self.__class__.__name__)
+        self.__task_queue = task_queue
+        self.__context = context
+        self.__save_to_dir = get_from_config(self.__config, 'TUMBLR', 'save_to_dir')
+        threading.Thread.__init__(self)
         
-    def __process_photo_data(self):
-        self.__log.info('Start processing and downloading photos')
+    def __process_photo_data(self, photo_data):
         result_dict = dict()
-        for caption, photo_dict in self.__photo_data.items():
+        for caption, photo_dict in photo_data.items():
             sizes = photo_dict.keys()
             sizes.sort(reverse=True)
             if sizes:
@@ -120,6 +120,63 @@ class TumblrService(object):
                 self.__log.error('Error saving photo: %r. Error: %r', url, error)
         return False  
         
+    def run(self):
+        count_saved = 0
+        while True:
+            try:
+                if not self.__context.get('crawlers_stopped', False):
+                    photo_data = self.__task_queue.get(block=True)
+                    self.__log.info('Received %d photo_data from the queue', len(photo_data))
+                else:
+                    photo_data = self.__task_queue.get(block=True, timeout=5)
+                    self.__log.info('Received %d photo_data from the timeout queue', len(photo_data))
+                    
+                for caption, url_dict in self.__process_photo_data(photo_data).items():
+                    urls = url_dict.values()
+                    if urls:
+                        if self.__save_photo(caption, urls[0]):
+                            count_saved += 1
+                self.__task_queue.task_done()
+            except Queue.Empty:
+                with CONTEXT_LOCK:
+                    self.__context['saving_done'] = True
+                    break
+            self.__log.info('Saved %d photos so far', count_saved)
+            
+    
+class TumblrService(object):
+    
+    def __init__(self, config_file):
+        
+        self.__config_file = config_file
+        self.__config = ConfigParser.SafeConfigParser()
+        self.__config.read(config_file)
+        self.__get_logging()
+        self.__me_name = get_from_config(self.__config, 'TUMBLR', 'me_name')
+        self.__me__pasword = get_from_config(self.__config, 'TUMBLR', 'me_password')
+        self.__me_mail = get_from_config(self.__config, 'TUMBLR', 'me_email')
+        self.__me_max = get_from_config(self.__config, 'TUMBLR', 'me_max')
+        self.__acounts = [item.strip() for item in get_from_config(self.__config, 'TUMBLR', 'accounts').split(',')]
+        self.__limit = get_from_config(self.__config, 'TUMBLR', 'limit', 0)
+        self.__context = {'count_done_crawler': 0}
+        self.__task_queue = Queue.Queue(0)
+        self.__image_crawlers = list()
+        for account in self.__acounts:
+            self.__image_crawlers.append(TumblrImageCrawler(task_queue = self.__task_queue, 
+                                                            context = self.__context,
+                                                            name = account, 
+                                                            max = self.__me_max, 
+                                                            limit = self.__limit))
+        self.__save_thread = PhotoSaveThread(self.__config, self.__task_queue, self.__context)
+        self.__log.info('I am: %r', self.__class__.__name__)
+        
+    def __get_logging(self):
+        
+        logging.basicConfig(level = getattr(logging, self.__config.get('TUMBLR', 'logging_level')),
+                            format = '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+        self.__log = logging.getLogger(self.__class__.__name__)
+        
+        
     def __repr__(self):
         return "%r" % self.__dict__
         
@@ -128,19 +185,26 @@ class TumblrService(object):
         self.__log.info('Start running crawlers..')
         for thread in self.__image_crawlers:
             thread.start()
-        for thread in self.__image_crawlers:
-            thread.join()
-        self.__log.info('Finished collecting data')
-        self.__log.info('Collected %d items to photo process', len(self.__photo_data))
-        count_saved = 0
-        for caption, url_dict in self.__process_photo_data().items():
-            urls = url_dict.values()
-            if urls:
-                if self.__save_photo(caption, urls[0]):
-                    count_saved += 1
-        self.__log.info('Saved %d photos', count_saved)
-          
             
+        self.__log.info('Start saving thread..')
+        self.__save_thread.start()
+        
+        while True:
+            self.__log.info('Context: %r', self.__context)
+            with CONTEXT_LOCK:
+                if  self.__context.get('count_done_crawler') >= len(self.__acounts):
+                    self.__context['crawlers_stopped'] = True
+                    queue_length = self.__task_queue.qsize()
+                    self.__log.info('Queue size: %d', queue_length)
+                    if queue_length == 0:
+                        if self.__context.get('saving_done', False):
+                            self.__log.info('Saving done.')
+                            break
+            time.sleep(1)
+                  
+        self.__log.info('All Done.')  
+        sys.exit(0)
+           
             
 def main(config_file_path):
     
